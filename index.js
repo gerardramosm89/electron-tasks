ed93a03f7d72728ffbf7df24820d4ba07bcb1290
Gerard Ramos <gerard.ramosm89@gmail.com>
2017-07-17 18:51:45 -0700

    task

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 const electron = require('electron');
-
-const { app, BrowserWindow } = electron;
+const path = require('path');
+const { app, BrowserWindow, Tray } = electron;
 
 let mainWindow;
-
+let tray;
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
     height:500,
     width:300,
     frame: false,
-    resizable: false
+    resizable: false,
+    show: false
   });
   mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+  const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
+  const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
+  tray = new Tray(iconPath);
+  tray.on('click', (e, bounds) => {
+    // Click event bounds
+    const { x, y } = bounds;
+    const { height, width } = mainWindow.getBounds();
+
+    if (mainWindow.isVisible()) {
+      mainWindow.hide();
+    } else {
+      mainWindow.setBounds({
+        x: x - width / 2,
+        y: y,
+        height: height,
+        width: width
+      });
+      mainWindow.show();
+    }
+  });
 });
